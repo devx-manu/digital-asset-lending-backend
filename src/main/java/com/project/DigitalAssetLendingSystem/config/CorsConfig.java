63d5b4d7ffdcbc2b222fba0e0fdf678e0e323a24
Manu SH <devx.manu@gmail.com>
2026-05-08 20:51:33 +0530

    Update CORS configuration to allow new frontend origin
    Added a new allowed origin for CORS configuration.

--- src/main/java/com/project/DigitalAssetLendingSystem/config/CorsConfig.java
+++ src/main/java/com/project/DigitalAssetLendingSystem/config/CorsConfig.java
@@ -17,7 +17,8 @@ public class CorsConfig {
         CorsConfiguration configuration = new CorsConfiguration();
 
         configuration.setAllowedOrigins(
-                List.of("http://localhost:5173")
+                List.of("http://localhost:5173",
+                        "https://digital-asset-lending-frontend.vercel.app")
         );
 
         configuration.setAllowedMethods(
